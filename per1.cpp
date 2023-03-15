a63877bde22b447ad9ad022e5584e1b698e1118e
Iq11k <90621955+Iq11k@users.noreply.github.com>
2023-03-15 20:12:05 +0700

    Add files via upload

---
+++ per1.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstdlib>
+#include<iomanip>
+#include<time.h>
+using namespace std;
+
+int main(){
+    int     maxValues = 10;
+    double  rate = 0.75;
+    double  random;
+    double  randomValues[maxValues];
+
+    // generate random seed for srand
+    srand(time(0));
+
+
+    for (int i = 0; i < maxValues; i++){
+        random = (double)rand()/RAND_MAX; //generate random Number
+        randomValues[i] = random;
+        cout << fixed << setprecision(2) << randomValues[i] << endl;
+    }
+
+    // declare minimum and maximum variable
+    double minNum, maxNum;
+    
+    cout << endl;
+
+    // input the last number upper the rate to the minimum Number variable
+    for (int a = 0; a < maxValues; a++){
+        if (randomValues[a] > rate){
+            minNum = randomValues[a];
+        }
+    }
+
+    // find the minimum and maximum number
+    for (int a = 0; a < maxValues; a++){
+        if (randomValues[a] > rate){
+            cout << fixed << setprecision(2) << randomValues[a] << endl;
+            
+            // minimum Number
+            if (randomValues[a] < minNum){
+                minNum = randomValues[a];
+            }
+
+            // maximum Number
+            else if (randomValues[a] > maxNum){
+                maxNum = randomValues[a];
+            }
+        }
+    }
+
+    cout << "\nNilai max = " << maxNum << "\nNilai min = " << minNum;
+
+}
